dd8fffc449c0acf6949dc526a7c7eec160827135
Juan <jvelasco17@alumnos.utalca.cl>
2018-09-06 20:35:03 -0300

    creacion de cascaron de clases

---
+++ JavaFXApplication5/src/clases/Diagrama.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author IP-ROUTE
+ */
+public class Diagrama {
+    ArrayList<Entidad> entidades;
+    ArrayList<Relacion> relaciones;
+
+    public Diagrama() {
+        this.entidades = new ArrayList<>();
+        this.relaciones = new ArrayList<>(); 
+    }
+    
+}
