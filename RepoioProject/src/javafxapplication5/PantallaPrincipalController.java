4d47235f7d1871307a6bb0347d451b84a448af0f
usuario generico <j.pablo.velasc@gmail.com>
2018-09-08 15:22:52 -0300

    Se añade una hoja de estilo, se crean los controladores de la interfas

--- RepoioProject/src/javafxapplication5/PantallaPrincipalController.java
+++ RepoioProject/src/javafxapplication5/PantallaPrincipalController.java
@@ -7,7 +7,11 @@ package javafxapplication5;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -16,12 +20,34 @@ import javafx.fxml.Initializable;
  */
 public class PantallaPrincipalController implements Initializable {
 
+    @FXML
+    private Button btEntidad;
+    @FXML
+    private Button btRelacion;
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private Button btBorrar;
+
     /**
      * Initializes the controller class.
      */
 
+    @Override
     public void initialize(URL url, ResourceBundle rb) {
         
     }    
+
+    @FXML
+    private void crearEntidad(ActionEvent event) {
+    }
+
+    @FXML
+    private void crearRelacion(ActionEvent event) {
+    }
+
+    @FXML
+    private void borrar(ActionEvent event) {
+    }
     
 }
