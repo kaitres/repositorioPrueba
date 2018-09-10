e8b3809e32f915e24739e3c642bc1c700babd7b0
The.N <The.N@DESKTOP-OUQBPA4>
2018-09-09 23:42:12 -0300

    hexagono implementado

--- RepoioProject/src/clases/Hexagono.java
+++ RepoioProject/src/clases/Hexagono.java
@@ -5,10 +5,28 @@
  */
 package clases;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import java.util.ArrayList;
+
 /**
  *
  * @author IP-ROUTE
  */
 public class Hexagono extends Figura{
-    
+    public Hexagono(int centroX , int centroY , int escala){
+        this.coordenadas = new ArrayList<>();
+        //punto 1
+        this.coordenadas.add(new Coordenada((int) (centroX - (sqrt(pow(escala , 2) - pow(escala/2,2) ))) , centroY - escala/2));
+        //punto 2
+        this.coordenadas.add(new Coordenada((int) (centroX - (sqrt(pow(escala , 2) - pow(escala/2,2) ))) , centroY + escala/2));
+        //punto 3
+        this.coordenadas.add(new Coordenada(centroX , centroY + escala ));
+        //punto 4
+        this.coordenadas.add(new Coordenada((int) (centroX + (sqrt(pow(escala , 2) - pow(escala/2,2) ))), centroY + escala/2 ));
+        //punto 5
+        this.coordenadas.add(new Coordenada((int) (centroX + (sqrt(pow(escala , 2) - pow(escala/2,2) ))), centroY - escala/2 ));
+        //punto 6
+        this.coordenadas.add(new Coordenada(centroX , centroY - escala ));  
+    }   
 }
