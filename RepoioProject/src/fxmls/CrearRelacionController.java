36c2ce206db069faaeba7c7eb57252d8448b75b4
cmsan <cmsandoval98@hotmail.com>
2018-09-17 14:17:12 -0300

    Se pueden agregar entidades a las relaciones al crearlas, ademas las relaciones son dinamica

--- RepoioProject/src/fxmls/CrearRelacionController.java
+++ RepoioProject/src/fxmls/CrearRelacionController.java
@@ -5,13 +5,18 @@
  */
 package fxmls;
 
+import clases.Entidad;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -29,16 +34,38 @@ public class CrearRelacionController implements Initializable {
     @FXML
     private TextField nombre;
 
-    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    Alert alertName = new Alert(Alert.AlertType.INFORMATION);
+    Alert alertEmpty = new Alert(Alert.AlertType.INFORMATION);
+    
+    @FXML
+    private ListView<String> lista;
+    @FXML
+    private SplitMenuButton entChoicer;
 
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        alert.setTitle("Error");
-        alert.setHeaderText(null);
-        alert.setContentText("Debes ponerle un nombre");
+        for (Entidad e: InterfazController.diagrama.getEntidades()){
+            MenuItem m = new MenuItem(e.getNombre());
+            entChoicer.getItems().add(m);
+            m.setOnAction(new EventHandler<ActionEvent>(){
+                @Override public void handle(ActionEvent event) {
+                    lista.getItems().add(e.getNombre());
+                    m.setDisable(true);
+                    InterfazController.compRelacion.add(e);
+                }
+            });
+        }
+        
+        alertName.setTitle("Error");
+        alertName.setHeaderText(null);
+        alertName.setContentText("Debes ponerle un nombre");
+        alertEmpty.setTitle("Error");
+        alertEmpty.setHeaderText(null);
+        alertEmpty.setContentText("Debe tener elementos");
     }    
 
     @FXML
@@ -49,26 +76,29 @@ public class CrearRelacionController implements Initializable {
 
     @FXML
     private void aceptar(ActionEvent event) {
-        if (nombre.getText().length()!=0){
-            InterfazController.newRelacionNombre = nombre.getText();
-            InterfazController.relacionValidacion = true;
-            Stage stage = (Stage) aceBtn.getScene().getWindow();
-            stage.close();
+        if (nombre.getText().length()==0){
+            alertName.showAndWait();
+        } else if (InterfazController.compRelacion.isEmpty()){
+            alertEmpty.showAndWait();
         } else{
-            alert.showAndWait();
-        }
-        
-    }
-
-    @FXML
-    private void txtField(ActionEvent event) {
-        if (nombre.getText().length()!=0){
             InterfazController.newRelacionNombre = nombre.getText();
             InterfazController.relacionValidacion = true;
             Stage stage = (Stage) nombre.getScene().getWindow();
             stage.close();
+        }
+    }
+
+    @FXML
+    private void txtField(ActionEvent event) {
+        if (nombre.getText().length()==0){
+            alertName.showAndWait();
+        } else if (InterfazController.compRelacion.isEmpty()){
+            alertEmpty.showAndWait();
         } else{
-            alert.showAndWait();
+            InterfazController.newRelacionNombre = nombre.getText();
+            InterfazController.relacionValidacion = true;
+            Stage stage = (Stage) nombre.getScene().getWindow();
+            stage.close();
         }
     }
     
