84fde4333c63ae872a21bbe3dbbc028152a5ef99
ThePina <thenico0509@gmail.com>
2018-11-29 23:36:54 -0300

    agrupacion v1.0

--- RepoioProject/src/clases/Agrupacion.java
+++ RepoioProject/src/clases/Agrupacion.java
@@ -17,36 +17,14 @@ import javafx.scene.paint.Color;
  * @author The.N
  */
 public class Agrupacion {
-    ArrayList<Entidad> entidades = new ArrayList<>(); 
     ArrayList<Relacion> relaciones = new ArrayList<>();
-    ArrayList<Herencia> herencias = new ArrayList<>();
     ArrayList<Point2D> coordenadas = new ArrayList<>();
     ArrayList<Point2D> cordCuadradoMov = new ArrayList<>();
     
     
     private Point2D getMayor(){
-        double XMayor = entidades.get(0).getFigura().getCoordenadas().get(0).getX();
-        double YMayor = entidades.get(0).getFigura().getCoordenadas().get(0).getY();
-        for (Entidad entidad : entidades) {
-            for (Point2D cord : entidad.getFigura().getCoordenadas()) {
-                if(cord.getX()> XMayor){
-                    XMayor = cord.getX();
-                }
-                if(cord.getY()> YMayor){
-                    YMayor = cord.getY();
-                }  
-            }
-            for (Propiedad prop : entidad.getPropiedades()){
-                for (Point2D cord : prop.getElip().getCoordenadas()) {
-                    if(cord.getX()> XMayor){
-                        XMayor = cord.getX();
-                    }
-                    if(cord.getY()> YMayor){
-                        YMayor = cord.getY();
-                    }  
-                }
-            }
-        }
+        double XMayor = relaciones.get(0).getFigura().getCoordenadas().get(0).getX();
+        double YMayor = relaciones.get(0).getFigura().getCoordenadas().get(0).getY();
         for (Relacion relacion : relaciones) {
             for (Point2D cord : relacion.getFigura().getCoordenadas()) {
                 if(cord.getX()> XMayor){
@@ -56,34 +34,35 @@ public class Agrupacion {
                     YMayor = (int)cord.getY();
                 }
             }
+            for (Entidad entidad : relacion.getComponentes()) {
+                for (Point2D cord : entidad.getFigura().getCoordenadas()) {
+                    if(cord.getX()> XMayor){
+                        XMayor = cord.getX();
+                    }
+                    if(cord.getY()> YMayor){
+                        YMayor = cord.getY();
+                    }  
+                }
+                for (Propiedad prop : entidad.getPropiedades()){
+                    for (Point2D cord : prop.getElip().getCoordenadas()) {
+                        if(cord.getX()> XMayor){
+                            XMayor = cord.getX();
+                        }
+                        if(cord.getY()> YMayor){
+                            YMayor = cord.getY();
+                        }  
+                    }
+                }
+            }      
         }
         return new Point2D(XMayor, YMayor);
     }
-    
-    private Point2D getMenor(){
-        double XMenor = entidades.get(0).getFigura().getCoordenadas().get(0).getX();
-        double YMenor = entidades.get(0).getFigura().getCoordenadas().get(0).getY();
         
-        for (Entidad entidad : entidades) {
-            for (Point2D cord : entidad.getFigura().getCoordenadas()) {
-                if(cord.getX()< XMenor){
-                    XMenor = cord.getX();
-                }
-                if(cord.getY()< YMenor){
-                    YMenor = cord.getY();
-                }
-            }
-            for (Propiedad prop : entidad.getPropiedades()){
-                for (Point2D cord : prop.getElip().getCoordenadas()) {
-                    if(cord.getX()< XMenor){
-                        XMenor = cord.getX();
-                    }
-                    if(cord.getY()< YMenor){
-                        YMenor = cord.getY();
-                    }  
-                }
-            }
-        }
+        
+      
+    private Point2D getMenor(){
+        double XMenor = relaciones.get(0).getFigura().getCoordenadas().get(0).getX();
+        double YMenor = relaciones.get(0).getFigura().getCoordenadas().get(0).getY();
         for (Relacion relacion : relaciones) {
             for (Point2D cord : relacion.getFigura().getCoordenadas()) {
                 if(cord.getX()< XMenor){
@@ -93,9 +72,30 @@ public class Agrupacion {
                     YMenor = cord.getY();
                 }              
             }
+            for (Entidad entidad : relacion.getComponentes()) {
+                for (Point2D cord : entidad.getFigura().getCoordenadas()) {
+                    if(cord.getX()< XMenor){
+                        XMenor = cord.getX();
+                    }
+                    if(cord.getY()< YMenor){
+                        YMenor = cord.getY();
+                    }
+                }
+                for (Propiedad prop : entidad.getPropiedades()){
+                    for (Point2D cord : prop.getElip().getCoordenadas()) {
+                        if(cord.getX()< XMenor){
+                            XMenor = cord.getX();
+                        }
+                        if(cord.getY()< YMenor){
+                            YMenor = cord.getY();
+                        }  
+                    }
+                }
+            } 
         }
         return new Point2D(XMenor, YMenor);
     }
+
     private void obtenerCordenadas(){
         Point2D menor = getMenor();
         Point2D mayor = getMayor();
@@ -146,4 +146,20 @@ public class Agrupacion {
         gc.strokeLine(coordenadas.get(3).getX(), coordenadas.get(3).getY(), coordenadas.get(0).getX(), coordenadas.get(0).getY());
     }
     
+    public void movimiento(Point2D p , GraphicsContext gc){
+        Point2D pAux= new Point2D(cordCuadradoMov.get(2).getX()-cordCuadradoMov.get(0).getX() , cordCuadradoMov.get(2).getY()-cordCuadradoMov.get(0).getY());
+        int XDistancia = (int) (p.getX() - pAux.getX());
+        int YDistancia = (int) (p.getY() - pAux.getY());
+        for (Relacion relacion : relaciones) {
+            Point2D pCRAux = relacion.getFigura().getPuntoCentral();
+            relacion.getFigura().setPuntoCentral(new Point2D(pCRAux.getX() + XDistancia,pCRAux.getY() +  YDistancia));
+            for (Entidad entidad : relacion.getComponentes()) {
+                Point2D pCAAux = relacion.getFigura().getPuntoCentral();
+                entidad.getFigura().setPuntoCentral(new Point2D(pCRAux.getX() + XDistancia,pCRAux.getY() +  YDistancia));
+            }
+        }
+        dibujarMarco(gc);
+        dibujarCuadrado(gc, cordCuadradoMov);
+        
+    }
 }
